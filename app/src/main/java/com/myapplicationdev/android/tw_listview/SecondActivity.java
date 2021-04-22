b100121be1d3d895294c3fc8b5ac8df62816095a
19031965 <19031965@myrp.edu.sg>
2021-04-22 15:44:51 +0800

    wow2

--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -13,9 +13,9 @@ import java.util.ArrayList;
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
-    TextView tv;
+    TextView tvModule;
     ImageView iv;
-    ArrayList<Module> module;
+    ArrayList<Module> moduleList;
     ArrayAdapter aa;
 
 
@@ -25,29 +25,19 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         lv = (ListView) this.findViewById(R.id.lvModule);
-        tv = (TextView) findViewById(R.id.tvYear);
+        tvModule = (TextView) findViewById(R.id.tvYear);
 
         // create module objects
-        module = new ArrayList<Module>();
-        module.add(new Module("C208" , true));
-        module.add(new Module("C200" , false));
-        module.add(new Module("C346" , true));
+        moduleList = new ArrayList<Module>();
+        moduleList.add(new Module("C208" , true));
+        moduleList.add(new Module("C200" , false));
+        moduleList.add(new Module("C346" , true));
 
-        aa = new ModuleAdapter(this, R.layout.row, module);
+        aa = new ModuleAdapter(this, R.layout.row, moduleList);
         lv.setAdapter(aa);
 
-        Intent year = getIntent();
+        Intent i = getIntent();
 
-        for(int i = 0; i < module.size(); i++) {
-            String moduleName = module.get(i).getModule();
-            boolean isProg = module.get(i).isProg();
-            tv.setText(moduleName);
-            if (isProg == true){
-                iv.setImageResource(R.drawable.prog);
-            }else{
-                iv.setImageResource(R.drawable.nonprog);
-            }
-        }
 
     }
 }
