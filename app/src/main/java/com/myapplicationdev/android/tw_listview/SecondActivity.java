b9149945f9db5aaab9ab6afda84354100ad0c952
19031965 <19031965@myrp.edu.sg>
2021-04-22 15:37:46 +0800

    wow2

--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,20 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
-    TextView tvYear;
+    TextView tv;
+    ImageView iv;
+    ArrayList<Module> module;
+    ArrayAdapter aa;
 
 
     @Override
@@ -17,15 +24,30 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        lv = (ListView) this.findViewById(R.id.lvModules);
-        tvYear = (TextView) findViewById(R.id.tvYear);
+        lv = (ListView) this.findViewById(R.id.lvModule);
+        tv = (TextView) findViewById(R.id.tvYear);
 
-        Intent i = getIntent();
-        String year = i.getStringExtra("year");
-        tvYear.setText(year);
+        // create module objects
+        module = new ArrayList<Module>();
+        module.add(new Module("C208" , true));
+        module.add(new Module("C200" , false));
+        module.add(new Module("C346" , true));
+
+        aa = new ModuleAdapter(this, R.layout.row, module);
+        lv.setAdapter(aa);
+
+        Intent year = getIntent();
+
+        for(int i = 0; i < module.size(); i++) {
+            String moduleName = module.get(i).getModule();
+            boolean isProg = module.get(i).isProg();
+            tv.setText(moduleName);
+            if (isProg == true){
+                iv.setImageResource(R.drawable.prog);
+            }else{
+                iv.setImageResource(R.drawable.nonprog);
+            }
+        }
 
     }
-
-
-
 }
